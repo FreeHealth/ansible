44bf70d6b3fc130656cf6d4e08283223a8e65f33
Jerome Pinguet <jerome@jerome.cc>
2017-12-26 03:42:55 +0100

    initial commit of playbook to provision Ubuntu with ready to use testing environment

---
+++ ubuntu/roles/mariadb/files/mysql.sh
@@ -0,0 +1,4 @@
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+echo "mysql-server-5.7 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get -y install mysql-server-5.7
